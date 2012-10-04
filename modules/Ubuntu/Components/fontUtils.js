6bb159f92be0f70668a0427ad9fae4210fef278b
Florian Boucault <florian@boucault.net>
2012-10-04 20:38:37 +0100

    fontUtils.js: no pragma library.

--- modules/Ubuntu/Components/fontUtils.js
+++ modules/Ubuntu/Components/fontUtils.js
@@ -14,8 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-//.pragma library
-
 function sizeToPixels(size) {
     var baseSize = units.dp(14)
     return Math.round(modularScale(size) * baseSize)
