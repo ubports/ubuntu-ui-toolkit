33d2503af8d3175dfe61095599a22e579b4e6b92
tpeeters <tim.peeters@canonical.com>
2012-10-05 01:11:27 +0200

    better

--- generate_po.sh
+++ generate_po.sh
@@ -1,2 +1,2 @@
 #grep tr\( * | sed 's/.*"\(.*\)"[^"]*$/\1/' | sort | uniq
-grep tr\( * | sed 's/.*i18n\.tr("\(.*\)").*/___\1___/' | sort -u | grep \_\_\_ | sed 's/___\(.*\)___/msgid "\1"\nmsgstr "\1"\n/'
+grep tr\( * | sed 's/.*i18n\.tr("\(.*\)").*/___\1___/' | grep \_\_\_ | sort -u | sed 's/___\(.*\)___/msgid "\1"\nmsgstr "\1"\n/'
