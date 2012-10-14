44e3ff8783c5d3c86c1739b050b302cb7a7717bf
tpeeters <tim.peeters@canonical.com>
2012-10-14 20:48:45 +0200

    remove commented-out line of code

--- generate_po.sh
+++ generate_po.sh
@@ -1,2 +1,1 @@
-#grep tr\( * | sed 's/.*"\(.*\)"[^"]*$/\1/' | sort | uniq
 grep tr\( * | sed 's/.*i18n\.tr("\(.*\)").*/___\1___/' | grep \_\_\_ | sort -u | sed 's/___\(.*\)___/msgid "\1"\nmsgstr ""\n/'
