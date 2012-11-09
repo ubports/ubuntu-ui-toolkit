c078075a3a0187b53931d25b3c8d8e199cb2eee8
Juhapekka Piiroinen <juhapekka.piiroinen@canonical.com>
2012-11-09 14:52:05 +0200

    [tests] added benchmark support for ListItems

--- tests/unit/tst_components_benchmark/tst_components_benchmark.cpp
+++ tests/unit/tst_components_benchmark/tst_components_benchmark.cpp
@@ -28,7 +28,7 @@ class tst_components_benchmark: public QObject
     Q_OBJECT
 
 private Q_SLOTS:
-    void benchmark_creation_data() {
+    void benchmark_creation_components_data() {
         QTest::addColumn<QString>("fileName");
 
         QDir dir;
@@ -49,7 +49,42 @@ private Q_SLOTS:
         }
     }
 
-    void benchmark_creation() {
+    void benchmark_creation_components() {
+        QFETCH(QString, fileName);
+
+        qDebug() << "Loading" << fileName;
+
+        QQmlComponent component(&engine, fileName);
+        QObject *obj = component.create();
+        obj->deleteLater();
+
+        QBENCHMARK {
+            QObject *obj = component.create();
+            obj->deleteLater();
+        }
+    }
+    void benchmark_creation_listitems_data() {
+        QTest::addColumn<QString>("fileName");
+
+        QDir dir;
+        dir.setPath("../../../modules/Ubuntu/Components/ListItems");
+        QStringList nameFilters;
+        nameFilters << "*.qml";
+        dir.setNameFilters(nameFilters);
+        dir.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
+        dir.setSorting(QDir::Size | QDir::Reversed);
+
+        QFileInfoList list = dir.entryInfoList();
+
+        qDebug() << "Found" << list.size() << "tests.";
+
+        for (int i = 0; i < list.size(); ++i) {
+            QFileInfo fileInfo = list.at(i);
+            QTest::newRow(fileInfo.fileName().toLatin1()) << fileInfo.fileName();
+        }
+    }
+
+    void benchmark_creation_listitems() {
         QFETCH(QString, fileName);
 
         qDebug() << "Loading" << fileName;
