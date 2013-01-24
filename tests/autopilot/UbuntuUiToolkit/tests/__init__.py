eb20db1b14da65ce66190a7e1ac88611a7968b9f
Juhapekka Piiroinen <juhapekka.piiroinen@canonical.com>
2013-01-24 08:08:57 +0200

    [tests] moved support functions to parent

--- tests/autopilot/UbuntuUiToolkit/tests/__init__.py
+++ tests/autopilot/UbuntuUiToolkit/tests/__init__.py
@@ -11,6 +11,8 @@ from os import remove
 import os.path
 from tempfile import mktemp
 
+from autopilot.matchers import Eventually
+from testtools.matchers import Is, Not, Equals
 from autopilot.introspection.qt import QtIntrospectionTestMixin
 from autopilot.testcase import AutopilotTestCase
 
@@ -55,3 +57,44 @@ class UbuntuUiToolkitTestCase(AutopilotTestCase, QtIntrospectionTestMixin):
                 "qmlscene",
                 "-I", get_module_include_path(),
                 qml_path)
+
+    def checkListItem(self, itemText):
+        item = self.app.select_single('Standard',text=itemText)
+        self.assertThat(item, Not(Is(None)));
+
+    def getListItem(self, itemText):
+        return self.app.select_single('Standard',text=itemText)
+
+    def getWidgetLoaderAndListView(self):
+        widgetLoader = self.app.select_single(objectName="widgetLoader")
+        listView = self.app.select_single(objectName="widgetList")
+        self.assertThat(listView, Not(Is(None)));
+        self.assertThat(listView.visible, Eventually(Equals(True)));
+        return (widgetLoader, listView)
+
+    def loadItem(self, item):
+        widgetLoader = self.app.select_single(objectName="widgetLoader")
+        self.selectItem(item)
+        self.assertThat(widgetLoader.progress,Eventually(Equals(1.0)))
+        loadedPage = self.app.select_single(objectName=item)
+        self.assertThat(loadedPage, Not(Is(None)));
+        self.assertThat(loadedPage.visible, Eventually(Equals(True)));  
+
+    def drag(self, itemText, itemTextTo):
+        item = self.getListItem(itemText)
+        itemTo = self.getListItem(itemTextTo)
+
+        self.mouse.move_to_object(item)
+        self.mouse.press()
+        self.mouse.move_to_object(itemTo)
+        self.mouse.release()
+
+    def selectItem(self, itemText):
+        item = self.getListItem(itemText)
+        self.assertThat(item.selected, Eventually(Equals(False)))
+
+        self.mouse.move_to_object(item)
+        self.mouse.click()
+
+        self.assertThat(item.selected, Eventually(Equals(True)))
+
--- tests/autopilot/UbuntuUiToolkit/tests/componentshowcase/test_componentshowcase.py
+++ tests/autopilot/UbuntuUiToolkit/tests/componentshowcase/test_componentshowcase.py
@@ -20,49 +20,6 @@ class GenericTests(UbuntuUiToolkitTestCase):
 
     test_qml_file = "%s/%s.qml" % (os.path.dirname(os.path.realpath(__file__)),"../../../../../demos/ComponentShowcase")
 
-    def checkListItem(self, itemText):
-        item = self.app.select_single('Standard',text=itemText)
-        self.assertThat(item, Not(Is(None)));
-
-    def getListItem(self, itemText):
-        return self.app.select_single('Standard',text=itemText)
-
-    def getWidgetLoaderAndListView(self):
-        widgetLoader = self.app.select_single(objectName="widgetLoader")
-        listView = self.app.select_single(objectName="widgetList")
-        self.assertThat(listView, Not(Is(None)));
-        self.assertThat(listView.visible, Eventually(Equals(True)));
-        return (widgetLoader, listView)
-
-    def loadItem(self, item):
-        widgetLoader = self.app.select_single(objectName="widgetLoader")
-        self.selectItem(item)
-        self.assertThat(widgetLoader.progress,Eventually(Equals(1.0)))
-        loadedPage = self.app.select_single(objectName=item)
-        self.assertThat(loadedPage, Not(Is(None)));
-        self.assertThat(loadedPage.visible, Eventually(Equals(True)));  
-
-    def drag(self, itemText, itemTextTo):
-        item = self.getListItem(itemText)
-        itemTo = self.getListItem(itemTextTo)
-
-        self.mouse.move_to_object(item)
-        self.mouse.press()
-        self.mouse.move_to_object(itemTo)
-        self.mouse.release()
-
-    def selectItem(self, itemText):
-        item = self.getListItem(itemText)
-        self.assertThat(item.selected, Eventually(Equals(False)))
-
-        self.mouse.move_to_object(item)
-        self.mouse.click()
-
-        self.assertThat(item.selected, Eventually(Equals(True)))
-
-    ###########################################################
-    # Tests
-
     def test_0_can_select_mainwindow(self):
         """Must be able to select the main window."""
 
