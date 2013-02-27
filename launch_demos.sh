400c22d78d3fa9cc3bb570b6d9364f45642fc5a3
Timo Jyrinki <timo.jyrinki@canonical.com>
2013-02-27 09:02:22 +0200

    Deep qmlscene path is needed if not depending strictly on qt5-default

--- launch_demos.sh
+++ launch_demos.sh
@@ -22,12 +22,12 @@ then
 	GRID_UNIT_PX=18 qmlscene --fullscreen -I modules demos/PhoneComponentShowcase.qml
 elif [ "$1" == "--autopilot" ]
 then
-	qmlscene -I modules demos/ComponentShowcase.qml -testability &
+	/usr/lib/*/qt5/bin/qmlscene -I modules demos/ComponentShowcase.qml -testability &
 	autopilot vis
 elif [ "$1" == "--autopilot-phone" ]
 then
 	GRID_UNIT_PX=18 qmlscene -I modules demos/PhoneComponentShowcase.qml -testability &
 	autopilot vis
 else
-	qmlscene -I modules demos/ComponentShowcase.qml
+	/usr/lib/*/qt5/bin/qmlscene -I modules demos/ComponentShowcase.qml
 fi
