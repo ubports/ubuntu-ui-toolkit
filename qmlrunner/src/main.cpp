b9d5b9e233043a05ead0e95ae74f4a9bd86adbfa
Timo Jyrinki <timo.jyrinki@canonical.com>
2013-05-07 17:37:50 +0300

    Fix build error

--- qmlrunner/src/main.cpp
+++ qmlrunner/src/main.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2012 Canonical Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Florian Boucault <florian.boucault@canonical.com>
+ */
+
 #include <QtCore/QLatin1String>
 //#include <QtWidgets/QApplication>
 #include <QtGui/QGuiApplication>
@@ -21,7 +39,7 @@ int main(int argc, char** argv)
     }
 
     QQuickView window;
-    window.setWindowFlags(Qt::Window | Qt::WindowSystemMenuHint | Qt::WindowTitleHint | Qt::WindowMinMaxButtonsHint | Qt::WindowCloseButtonHint);
+    window.setFlags(Qt::Window | Qt::WindowSystemMenuHint | Qt::WindowTitleHint | Qt::WindowMinMaxButtonsHint | Qt::WindowCloseButtonHint);
     QObject::connect(window.engine(), SIGNAL(quit()), qApp, SLOT(quit()));
     window.setResizeMode(QQuickView::SizeRootObjectToView);
     window.setSource(QUrl(app.arguments().value(1)));
