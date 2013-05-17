ce72a0db96ad18294e916801be897b3aa8b08675
Zsombor Egri <zsombor.egri@canonical.com>
2013-05-17 10:03:20 +0300

    optimize import

--- modules/Ubuntu/Components/componentUtils.js
+++ modules/Ubuntu/Components/componentUtils.js
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+.pragma library
 /*
   This cannot be made as standalone library as from those we cannot access the
   attached properties.
