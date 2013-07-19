bbf55f7be39eb1a645c751f8f0cd278360e70758
Leo Arias <leo.arias@canonical.com>
2013-07-19 00:28:20 -0600

    Added the Tabs autopilot emulator.

--- tests/autopilot/UbuntuUiToolkit/tests/test_emulators.py
+++ tests/autopilot/UbuntuUiToolkit/tests/test_emulators.py
@@ -44,6 +44,19 @@ MainView {
         self.assertIsInstance(toolbar, emulators.Toolbar)
         self.assertFalse(toolbar.opened)
 
+    def test_get_tabs_without_tabs(self):
+        error = self.assertRaises(
+            AssertionError, self.main_view.get_tabs)
+        self.assertEqual(
+            error.message, 'The MainView has no Tabs.')
+
+    def test_switch_to_next_tab_without_tabs(self):
+        header = self.main_view.get_header()
+        error = self.assertRaises(
+            AssertionError, header.switch_to_next_tab)
+        self.assertEqual(
+            error.message, 'The MainView has no Tabs.')
+
 
 class PageTestCase(tests.UbuntuUiToolkitTestCase):
 
@@ -61,8 +74,9 @@ MainView {
 }
 """)
 
-    def test_get_header(self):
+    def test_header_custom_emulator(self):
         header = self.main_view.get_header()
+        self.assertIsInstance(header, emulators.Header)
         self.assertTrue(header.visible)
         self.assertEqual(header.title, "Test title")
 
@@ -137,6 +151,101 @@ MainView {
     def test_click_unexisting_button(self):
         toolbar = self.main_view.open_toolbar()
         error = self.assertRaises(
-            AssertionError, toolbar.click_button, 'unexisting')
+            ValueError, toolbar.click_button, 'unexisting')
         self.assertEqual(
             error.message, 'Button with objectName "unexisting" not found.')
+
+
+class TabsTestCase(tests.UbuntuUiToolkitTestCase):
+
+    test_qml = ("""
+import QtQuick 2.0
+import Ubuntu.Components 0.1
+import Ubuntu.Components.ListItems 0.1 as ListItem
+
+MainView {
+    width: units.gu(48)
+    height: units.gu(60)
+
+    Tabs {
+        id: tabs
+        Tab {
+            objectName: "tab1"
+            title: "Test tab 1"
+        }
+        Tab {
+            objectName: "tab2"
+            title: "Test tab 2"
+        }
+    }
+}
+""")
+
+    def test_tabs_custom_emulator(self):
+        self.assertIsInstance(self.main_view.get_tabs(), emulators.Tabs)
+
+    def test_get_current_tab(self):
+        tabs = self.main_view.get_tabs()
+        self.assertEqual(tabs.get_current_tab().title, 'Test tab 1')
+
+    def test_switch_to_next_tab_from_header(self):
+        header = self.main_view.get_header()
+        header.switch_to_next_tab()
+        current_tab = self.main_view.get_tabs().get_current_tab()
+        self.assertEqual(current_tab.title, 'Test tab 2')
+
+    def test_switch_to_next_tab_from_main_view(self):
+        current_tab = self.main_view.switch_to_next_tab()
+        self.assertEqual(current_tab.title, 'Test tab 2')
+
+    def test_switch_to_next_tab_from_last(self):
+        last_tab_index = 1
+        self.main_view.switch_to_tab_by_index(last_tab_index)
+        current_tab = self.main_view.switch_to_next_tab()
+        self.assertEqual(current_tab.title, 'Test tab 1')
+
+    def test_switch_to_tab_by_index(self):
+        current_tab = self.main_view.switch_to_tab_by_index(1)
+        self.assertEqual(current_tab.title, 'Test tab 2')
+        current_tab = self.main_view.switch_to_tab_by_index(0)
+        self.assertEqual(current_tab.title, 'Test tab 1')
+
+    def test_switch_to_opened_tab_is_not_opened_again(self):
+        with mock.patch.object(
+                emulators.Header, 'switch_to_next_tab') as mock_switch:
+            current_tab = self.main_view.switch_to_tab_by_index(0)
+
+        self.assertFalse(mock_switch.called)
+        self.assertEqual(current_tab.title, 'Test tab 1')
+
+    def test_get_number_of_tabs(self):
+        tabs = self.main_view.get_tabs()
+        self.assertEqual(tabs.get_number_of_tabs(), 2)
+
+    def test_swith_to_tab_by_index_out_of_range(self):
+        last_tab_index = 1
+        error = self.assertRaises(
+            IndexError, self.main_view.switch_to_tab_by_index,
+            last_tab_index + 1)
+        self.assertEqual(error.message, 'Tab index out of range.')
+
+    def test_switch_to_previous_tab_from_first(self):
+        current_tab = self.main_view.switch_to_previous_tab()
+        self.assertEqual(current_tab.title, 'Test tab 2')
+
+    def test_switch_to_previous_tab_not_from_first(self):
+        self.main_view.switch_to_tab_by_index(1)
+        current_tab = self.main_view.switch_to_previous_tab()
+        self.assertEqual(current_tab.title, 'Test tab 1')
+
+    def test_switch_to_tab_by_object_name(self):
+        current_tab = self.main_view.switch_to_tab('tab2')
+        self.assertEqual(current_tab.title, 'Test tab 2')
+        current_tab = self.main_view.switch_to_tab('tab1')
+        self.assertEqual(current_tab.title, 'Test tab 1')
+
+    def test_switch_to_unexisting_tab(self):
+        error = self.assertRaises(
+            ValueError, self.main_view.switch_to_tab, 'unexisting')
+        self.assertEqual(
+            error.message, 'Tab with objectName "unexisting" not found.')
