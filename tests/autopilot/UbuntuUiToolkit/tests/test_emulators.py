1b6f3fce6f2201605a062da9a41ff9b3e328244c
Leo Arias <leo.arias@canonical.com>
2013-07-19 00:01:09 -0600

    Added the Toolbar Autopilot emulator.

--- tests/autopilot/UbuntuUiToolkit/tests/test_emulators.py
+++ tests/autopilot/UbuntuUiToolkit/tests/test_emulators.py
@@ -8,17 +8,44 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import mock
 
-from UbuntuUiToolkit import emulators, tests
+from UbuntuUiToolkit import tests
+from UbuntuUiToolkit import emulators
 
 
 class MainViewTestCase(tests.UbuntuUiToolkitTestCase):
+
+    test_qml = ("""
+import QtQuick 2.0
+import Ubuntu.Components 0.1
+
+MainView {
+    width: units.gu(48)
+    height: units.gu(60)
+}
+""")
+
+    def test_main_view_custom_emulator(self):
+        self.assertIsInstance(self.main_view, emulators.MainView)
+
+    def test_get_header_without_header(self):
+        header = self.main_view.get_header()
+        self.assertFalse(header.visible)
+
+    def test_toolbar_custom_emulator(self):
+        toolbar = self.main_view.get_toolbar()
+        self.assertIsInstance(toolbar, emulators.Toolbar)
+        self.assertFalse(toolbar.opened)
+
+
+class PageTestCase(tests.UbuntuUiToolkitTestCase):
 
     test_qml = ("""
 import QtQuick 2.0
@@ -34,9 +61,82 @@ MainView {
 }
 """)
 
-    def test_main_view_custom_emulator(self):
-        self.assertIsInstance(self.main_view, emulators.MainView)
-
     def test_get_header(self):
         header = self.main_view.get_header()
+        self.assertTrue(header.visible)
         self.assertEqual(header.title, "Test title")
+
+
+class ToolbarTestCase(tests.UbuntuUiToolkitTestCase):
+
+    test_qml = ("""
+import QtQuick 2.0
+import Ubuntu.Components 0.1
+
+MainView {
+    width: units.gu(50)
+    height: units.gu(50)
+
+    Page {
+
+        Label {
+            id: "label"
+            objectName: "clicked_label"
+            anchors.centerIn: parent
+            text: "Button not clicked."
+        }
+
+        tools: ToolbarItems {
+            ToolbarButton {
+                objectName: "buttonName"
+                action: Action {
+                    text: "buttonText"
+                    onTriggered: label.text = "Button clicked."
+                }
+            }
+            locked: false
+            opened: false
+        }
+    }
+}
+""")
+
+    def test_open_toolbar(self):
+        toolbar = self.main_view.open_toolbar()
+        self.assertTrue(toolbar.opened)
+
+    def test_opened_toolbar_is_not_opened_again(self):
+        toolbar = self.main_view.open_toolbar()
+        with mock.patch.object(
+                self.main_view.pointing_device, 'drag') as mock_drag:
+            self.main_view.open_toolbar()
+
+        self.assertFalse(mock_drag.called)
+        self.assertTrue(toolbar.opened)
+
+    def test_close_toolbar(self):
+        toolbar = self.main_view.open_toolbar()
+        self.main_view.close_toolbar()
+        self.assertFalse(toolbar.opened)
+
+    def test_closed_toolbar_is_not_closed_again(self):
+        with mock.patch.object(
+                self.main_view.pointing_device, 'drag') as mock_drag:
+            self.main_view.close_toolbar()
+
+        self.assertFalse(mock_drag.called)
+        self.assertFalse(self.main_view.get_toolbar().opened)
+
+    def test_click_toolbar_button(self):
+        label = self.app.select_single('Label', objectName='clicked_label')
+        self.assertNotEqual(label.text, 'Button clicked.')
+        toolbar = self.main_view.open_toolbar()
+        toolbar.click_button('buttonName')
+        self.assertEqual(label.text, 'Button clicked.')
+
+    def test_click_unexisting_button(self):
+        toolbar = self.main_view.open_toolbar()
+        error = self.assertRaises(
+            AssertionError, toolbar.click_button, 'unexisting')
+        self.assertEqual(
+            error.message, 'Button with objectName "unexisting" not found.')
