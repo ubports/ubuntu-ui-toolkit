105a46afc76752b376d1aae9ef77953886a6bcbb
Christian Dywan <christian.dywan@canonical.com>
2013-09-24 15:58:24 +0200

    Add a verbose wrapper to make API tests more visible

---
+++ tests/qmlapicheck.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env sh
+#
+# Copyright 2013 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+QML="modules/Ubuntu/Components/*.qml modules/Ubuntu/Components/*/*.qml"
+CPP="plugins.qmltypes"
+
+echo Dumping QML API of C++ components
+qmlplugindump Ubuntu.Components 0.1 modules > plugins.qmltypes
+STATUS=$?
+test $STATUS = 0 || ERRORS=1
+test $STATUS = 0 || echo $RESULTS
+
+echo Running QML API check
+python tests/qmlapicheck.py $QML $CPP > components.api.new
+STATUS=$?
+test $STATUS = 0 || ERRORS=1
+test $STATUS = 0 || echo $RESULTS
+
+echo Verifying the diff between existing and generated API
+diff -Fqml -u components.api components.api.new
+STATUS=$?
+test $STATUS = 0 || ERRORS=1
+test $STATUS = 0 || echo $RESULTS
+
+if [ "x$ERRORS" != "x1" ]; then
+    echo API is all fine.
+    exit 0
+else
+    echo API test failed with errors. Did you forget to update components.api?
+    exit 1
+fi
--- tests/unit/add_qmlmakecheck.pri
+++ tests/unit/add_qmlmakecheck.pri
@@ -8,6 +8,5 @@ for(TEST, TESTS) {
   check.commands += QML2_IMPORT_PATH=../../../modules:$$QML2_IMPORT_PATH UBUNTU_UI_TOOLKIT_THEMES_PATH=../../../modules qmltestrunner -platform minimal -input $${TEST} -import "../../../modules" -o ../../$${TEST}.xml,xunitxml -o -,txt;
 }
 check.commands += cd ../../..;
-check.commands += qmlplugindump Ubuntu.Components 0.1 modules > plugins.qmltypes;
-check.commands += python tests/qmlapicheck.py modules/Ubuntu/Components/*.qml modules/Ubuntu/Components/*/*.qml plugins.qmltypes > components.api.new;
-check.commands += diff -Fqml -u components.api components.api.new || exit 1; cd tests/unit
+check.commands += sh tests/qmlapicheck.sh || exit 1;
+check.commands += cd tests/unit
