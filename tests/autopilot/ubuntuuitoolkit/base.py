934bdc20bcf94103325947879b0d1e26b506b7fd
Leo Arias <leo.arias@canonical.com>
2013-10-04 00:10:07 -0600

    Added a comment to the reason for not using the qmlscene binary directly. Made it a public method to be used by other apps.

--- tests/autopilot/ubuntuuitoolkit/base.py
+++ tests/autopilot/ubuntuuitoolkit/base.py
@@ -16,11 +16,23 @@
 
 """Base classes for Autopilot tests using the Ubuntu UI Toolkit."""
 
+import subprocess
+
 from autopilot import (
     input,
     platform,
     testcase
 )
+
+
+def get_qmlscene_launch_command():
+    """Return the command to launch qmlscene for autopilot tests."""
+    # We need to specify qt5 because qtchooser doesn't have a default
+    # configuration on devices and it seems the environment variable
+    # QT_SELECT=qt5 doesn't work for autopilot tests. --mirv - 2013-10-03
+    arch = subprocess.check_output(
+        ["dpkg-architecture", "-qDEB_HOST_MULTIARCH"]).strip()
+    return '/usr/lib/' + arch + '/qt5/bin/qmlscene'
 
 
 class UbuntuUIToolkitAppTestCase(testcase.AutopilotTestCase):
