179a932547340b751e0c6fa8655acd49c139a5d7
Christian Dywan <christian.dywan@canonical.com>
2013-10-31 12:06:43 +0100

    Your own code should carry your own name, makes sense

--- tests/unit_x11/tst_orientation/tst_orientation.cpp
+++ tests/unit_x11/tst_orientation/tst_orientation.cpp
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Author: Zsombor Egri <zsombor.egri@canonical.com>
+ * Author: Christian Dywan <christian.dywan@canonical.com>
  */
 
 #include <QtTest/QtTest>
