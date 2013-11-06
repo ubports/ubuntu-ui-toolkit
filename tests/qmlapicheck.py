462d8e5c8c231baac01733bd676e9818d967da9f
Christian Dywan <christian.dywan@canonical.com>
2013-11-06 14:20:39 +0100

    Implement BUILTINS in qmlapicheck to exclude built-in types

--- tests/qmlapicheck.py
+++ tests/qmlapicheck.py
@@ -19,25 +19,27 @@
 
 import sys
 import fileinput
+import os
 
 if len(sys.argv) < 2 or '-h' in sys.argv or '--help' in sys.argv:
     import os
 
     basename = os.path.basename(sys.argv[0])
     print (
-        'Usage:\n  %s FILENAME [FILENAME2..N]\n\n'
+        'Usage:\n  env BUILTINS=foo,bar %s FILENAME [FILENAME2..N]\n\n'
         '  Generate a QML API file\n'
         'Example:\n'
-        '  %s modules/Ubuntu/Components/*.qml plugins.qmltypes'
+        '  env BUILTINS=QQuick,QQml,U1db:: %s modules/Ubuntu/Components/*.qml plugins.qmltypes'
         ' > components.api.new\n'
         '  diff -Fqml -u components.api{,.new}\n' % (basename, basename))
     sys.exit(1)
 
+builtins = os.getenv('BUILTINS', '').split(',')
 for line in fileinput.input():
     # New file
     if fileinput.isfirstline():
         in_block = 0
-        in_comment = False
+        in_comment = in_builtin_type = False
         annotated_type = None
         if fileinput.filename()[-3:] == 'qml':
             filetype = 'qml'
@@ -71,18 +73,31 @@ for line in fileinput.input():
         continue
 
     if '{' in line and '}' in line:
-        if filetype == 'qmltypes':
+        if filetype == 'qmltypes' and not in_builtin_type:
             print('    ' + line.strip())
         continue
 
     # End of function/ signal/ Item block
     if '}' in line:
         in_block -= 1
+        if in_block == 1 and in_builtin_type:
+            in_builtin_type = False
         continue
 
     # Only root "Item {" is inspected for QML, otherwise all children
     if in_block == 1 or filetype == 'qmltypes':
         words = line.strip().split(' ')
+        # Skip types with prefixes considered builtin
+        if len(words) > 1 and words[0] == 'name:':
+            found_builtin_type = False
+            for builtin in builtins:
+                if words[1].startswith('"' + builtin):
+                    found_builtin_type = True
+                    break
+            if found_builtin_type:
+                in_builtin_type = True
+                continue
+
         if filetype == 'qmltypes' and in_block > 1:
             keywords.append('name:')
             keywords.append('Parameter')
@@ -100,7 +115,8 @@ for line in fileinput.input():
                     annotated_type = None
                 elif filetype == 'qmltypes':
                     signature = line.strip()
-                print('    %s' % (signature))
+                if not in_builtin_type:
+                    print('    %s' % (signature))
                 break
 
     # Start of function/ signal/ Item block
