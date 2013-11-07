18d875ef4fc273f85b1b1aa6f2e47d9c30dc0345
Christian Dywan <christian.dywan@canonical.com>
2013-11-07 14:13:36 +0100

    Move Xvfb run into a script and use traps for error handling

---
+++ tests/xvfb.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#
+# Copyright 2013 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Christian Dywan <christian.dywan@canonical.com>
+
+# xvfb-run can't load gl drivers, Xvfb can
+DISPLAY=:5
+trap : USR1
+(trap '' USR1; exec Xvfb $DISPLAY -screen 0 1280x1024x24) &
+EPID=$!
+echo Running $* in virtual frame buffer with $EPID…
+wait $EPID || :
+if kill -0 $EPID; then
+    echo Executing test…
+    env DISPLAY=$DISPLAY $*
+    RETVAL=$?
+    echo Terminating…
+    kill -9 $EPID
+    echo Finished returning $RETVAL…
+    exit $?
+else
+    echo Xvfb failed to start…
+    exit 1
+fi
