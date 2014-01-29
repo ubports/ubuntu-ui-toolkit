6548caaffc23aed1f70078938e71feac1d51c010
Leo Arias <leo.arias@canonical.com>
2014-01-29 12:49:12 -0600

    Use the autopilot textfield helper.

--- tests/autopilot/ubuntuuitoolkit/tests/gallery/test_textinput.py
+++ tests/autopilot/ubuntuuitoolkit/tests/gallery/test_textinput.py
@@ -1,6 +1,6 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 #
-# Copyright (C) 2012, 2013 Canonical Ltd.
+# Copyright (C) 2012, 2013, 2014 Canonical Ltd.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -18,6 +18,8 @@
 
 from autopilot.matchers import Eventually
 from testtools.matchers import Equals
+
+from ubuntuuitoolkit import emulators
 from ubuntuuitoolkit.tests.gallery import GalleryTestCase
 
 
@@ -28,10 +30,11 @@ class TextInputTests(GalleryTestCase):
         item = "Text Field"
         self.loadItem(item)
         self.checkPageHeader(item)
-        textfield_standard = self.getObject('textfield_standard')
+        textfield_standard = self.main_view.select_single(
+            emulators.TextField, objectName='textfield_standard')
         self.pointing_device.click_object(textfield_standard)
         self.assertThat(textfield_standard.focus, Eventually(Equals(True)))
-        self.type_string('Hello World')
+        textfield_standard.write('Hello World')
         self.assertThat(textfield_standard.text,
                         Eventually(Equals('Hello World')))
 
@@ -39,14 +42,15 @@ class TextInputTests(GalleryTestCase):
         item = "Text Field"
         self.loadItem(item)
         self.checkPageHeader(item)
-        textfield_password = self.getObject('textfield_password')
+        textfield_password = self.main_view.select_single(
+            emulators.TextField, objectName='textfield_password')
         self.pointing_device.click_object(textfield_password)
         self.assertThat(textfield_password.text, Equals('password'))
 
         self.tap_clearButton('textfield_password')
         self.assertThat(textfield_password.text, Eventually(Equals('')))
 
-        self.type_string('abcdefgh123')
+        textfield_password.write('abcdefgh123')
         self.assertThat(textfield_password.text,
                         Eventually(Equals('abcdefgh123')))
 
@@ -54,20 +58,22 @@ class TextInputTests(GalleryTestCase):
         item = "Text Field"
         self.loadItem(item)
         self.checkPageHeader(item)
-        textfield_numbers = self.getObject('textfield_numbers')
+        textfield_numbers = self.main_view.select_single(
+            emulators.TextField, objectName='textfield_numbers')
         self.assertThat(textfield_numbers.text, Eventually(Equals('123')))
 
-        self.tap_clearButton('textfield_numbers')
+        textfield_numbers.clear()
         self.assertThat(textfield_numbers.text, Eventually(Equals('')))
         #try typing decimal value when text filed is int only.
-        self.type_string('-100.123')
+        textfield_numbers.write('-100.123')
         self.assertThat(textfield_numbers.text, Eventually(Equals('-100123')))
 
     def test_textfield_disabled(self):
         item = "Text Field"
         self.loadItem(item)
         self.checkPageHeader(item)
-        textfield_disabled = self.getObject('textfield_disabled')
+        textfield_disabled = self.main_view.select_single(
+            emulators.TextField, objectName='textfield_disabled')
         self.assertFalse(textfield_disabled.enabled)
         #try tapping a disabled field and verify that focus is false.
         self.pointing_device.click_object(textfield_disabled)
