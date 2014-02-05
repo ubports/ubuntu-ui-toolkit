6d9850d9d61539cbbd7ff005fa6fdc9a9ac3d4f4
Zsombor Egri <zsombor.egri@canonical.com>
2014-02-06 01:50:05 +0200

    settings construction fixed

--- modules/Ubuntu/Components/plugin/statesaverbackend_p.cpp
+++ modules/Ubuntu/Components/plugin/statesaverbackend_p.cpp
@@ -62,7 +62,7 @@ void StateSaverBackend::initialize()
     }
     m_archive = new QSettings(QString("%1/%2.state")
                               .arg(QStandardPaths::standardLocations(QStandardPaths::TempLocation)[0])
-                              .arg(applicationName));
+                              .arg(applicationName), QSettings::NativeFormat);
     m_archive->setFallbacksEnabled(false);
 }
 
