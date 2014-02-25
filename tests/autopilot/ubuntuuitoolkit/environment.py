494c18f0bcb5ca0ca7bf6a4f18b92dfa1c23ac8b
Leo Arias <leo.arias@canonical.com>
2014-02-25 08:37:40 -0600

    Added initctl environment variable helpers for autopilot tests.

---
+++ tests/autopilot/ubuntuuitoolkit/environment.py
@@ -0,0 +1,61 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Copyright (C) 2014 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import subprocess
+
+from autopilot import logging as autopilot_logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def is_initctl_global_env_var_set(variable):
+    """Check True if an initctl global environment variable is set.
+
+    :param variable: The name of the variable to check.
+    :return: True if the variable is set. False otherwise.
+
+    """
+    try:
+        get_initctl_global_env_var(variable)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+def get_initctl_global_env_var(variable):
+    """Return the value of an initctl global environment variable."""
+    output = subprocess.check_output(
+        ['/sbin/initctl', 'get-env', '--global', variable],
+        stderr=subprocess.STDOUT)
+    return output.rstrip()
+
+
+@autopilot_logging.log_action(logger.info)
+def set_initctl_global_env_var(variable, value):
+    """Set the value of an initctl global environment variable."""
+    subprocess.call(
+        ['/sbin/initctl', 'set-env', '--global', '%s=%s' % (variable, value)],
+        stderr=subprocess.STDOUT)
+
+
+@autopilot_logging.log_action(logger.info)
+def unset_initctl_global_env_var(variable):
+    """Remove an initctl global environment variable."""
+    subprocess.call(
+        ['/sbin/initctl', 'unset-env', '--global', variable],
+        stderr=subprocess.STDOUT)
---
+++ tests/autopilot/ubuntuuitoolkit/tests/test_environment.py
@@ -0,0 +1,57 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Copyright (C) 2014 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import testtools
+
+from ubuntuuitoolkit import environment
+
+
+class InitctlEnvironmentVariableTestCase(testtools.TestCase):
+
+    def test_is_environment_variable_set_with_unset_variable(self):
+        variable = 'I do not exist'
+        self.assertFalse(environment.is_initctl_global_env_var_set(variable))
+
+    def test_is_environment_variable_set_with_set_variable(self):
+        variable = 'Testvariabletoset'
+        self.addCleanup(environment.unset_initctl_global_env_var, variable)
+
+        environment.set_initctl_global_env_var(variable, 'dummy')
+
+        self.assertTrue(environment.is_initctl_global_env_var_set(variable))
+
+    def test_get_environment_variable(self):
+        variable = 'Testvariabletoget'
+        self.addCleanup(environment.unset_initctl_global_env_var, variable)
+        environment.set_initctl_global_env_var(variable, 'test value')
+
+        self.assertEqual(
+            'test value', environment.get_initctl_global_env_var(variable))
+
+    def test_unset_environment_variable(self):
+        variable = 'Testvariabletoget'
+        environment.set_initctl_global_env_var(variable, 'dummy')
+
+        environment.unset_initctl_global_env_var(variable)
+
+        self.assertFalse(environment.is_initctl_global_env_var_set(variable))
+
+    def test_unset_environment_variable_with_unset_variable(self):
+        variable = 'I do not exist'
+
+        environment.unset_initctl_global_env_var(variable)
+
+        self.assertFalse(environment.is_initctl_global_env_var_set(variable))
