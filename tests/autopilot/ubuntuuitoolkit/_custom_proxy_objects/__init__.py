a2045efd12d4de6b24d1db1e9ee4c13fb9a13ca2
Leo Arias <leo.arias@canonical.com>
2014-04-10 22:39:45 -0600

    Added a _custom_proxy_objects module.

--- tests/autopilot/ubuntuuitoolkit/_custom_proxy_objects/__init__.py
+++ tests/autopilot/ubuntuuitoolkit/_custom_proxy_objects/__init__.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-"""Ubuntu UI Toolkit autopilot tests and helpers."""
+"""Ubuntu UI Toolkit Autopilot custom proxy objects."""
 
 import logging
 from distutils import version
