63e5fde9dbf179ae0ca18cfb89ba7d157e3b5d30
Leo Arias <leo.arias@canonical.com>
2014-04-10 21:14:09 -0600

    Added a deprecated emulators module to keep compatibility.

---
+++ tests/autopilot/ubuntuuitoolkit/emulators.py
@@ -0,0 +1,55 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Copyright (C) 2012, 2013, 2014 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Emulators was the old name for the custom proxy objects."""
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+logger.warning(
+    'The ubuntuuitoolkit.emulators module is deprecated. Import the autopilot '
+    'helpers from the top-level ubuntuuitoolkit module.')
+
+
+from ubuntuuitoolkit import (  #NOQA
+    check_autopilot_version,
+    get_keyboard,
+    get_pointing_device,
+    ActionSelectionPopover,
+    Base,
+    CheckBox,
+    ComposerSheet,
+    Empty,
+    Flickable,
+    Header,
+    ItemSelector,
+    MainView,
+    MultiValue,
+    QQuickListView,
+    SingleControl,
+    SingleValue,
+    Standard,
+    Subtitled,
+    TabBar,
+    Tabs,
+    TextField,
+    Toolbar,
+    UbuntuUIToolkitException as ToolkitEmulatorException,
+    UbuntuUIToolkitCustomProxyObjectBase as UbuntuUIToolkitEmulatorBase,
+)
