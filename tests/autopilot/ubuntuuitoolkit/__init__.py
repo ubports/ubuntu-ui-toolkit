cefdea0f92320c9f31720777c6b8312c430241ed
Leo Arias <leo.arias@canonical.com>
2014-04-10 22:55:17 -0600

    Added the missing init.

---
+++ tests/autopilot/ubuntuuitoolkit/__init__.py
@@ -0,0 +1,20 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Copyright (C) 2012, 2013, 2014 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Ubuntu UI Toolkit Autopilot tests and helpers."""
+
+
+from ubuntuuitoolkit._custom_proxy_objects import *  # NOQA
