e3c0489cf9f4a342ef4c42ffea57a784756faec7
Leo Arias <leo.arias@canonical.com>
2014-04-16 19:06:31 -0600

    Split the tests.

---
+++ tests/autopilot/ubuntuuitoolkit/tests/custom_proxy_objects/test_listitems.py
@@ -0,0 +1,170 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Copyright (C) 2013, 2014 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import ubuntuuitoolkit
+from ubuntuuitoolkit import tests
+
+
+class SwipeToDeleteTestCase(tests.QMLStringAppTestCase):
+
+    test_qml = ("""
+import QtQuick 2.0
+import Ubuntu.Components 0.1
+import Ubuntu.Components.ListItems 0.1
+
+
+MainView {
+    width: units.gu(48)
+    height: units.gu(60)
+
+    Page {
+
+        ListModel {
+            id: testModel
+
+            ListElement {
+                name: "listitem_destroyed_on_remove_with_confirm"
+                label: "Item destroyed on remove with confirmation"
+                confirm: true
+            }
+            ListElement {
+                name: "listitem_destroyed_on_remove_without_confirm"
+                label: "Item destroyed on remove without confirmation"
+                confirm: false
+            }
+        }
+
+        Column {
+            anchors { fill: parent }
+
+            Standard {
+                objectName: "listitem_standard"
+                confirmRemoval: true
+                removable: true
+                text: 'Slide to remove'
+            }
+
+            Empty {
+                objectName: "listitem_empty"
+            }
+
+            Standard {
+                objectName: "listitem_without_confirm"
+                confirmRemoval: false
+                removable: true
+                text: "Item without delete confirmation"
+            }
+
+            ListView {
+                anchors { left: parent.left; right: parent.right }
+                height: childrenRect.height
+                model: testModel
+
+                delegate: Standard {
+                    removable: true
+                    confirmRemoval: confirm
+                    onItemRemoved: testModel.remove(index)
+                    text: label
+                    objectName: name
+                }
+            }
+        }
+    }
+}
+""")
+
+    def setUp(self):
+        super(SwipeToDeleteTestCase, self).setUp()
+        self._item = self.main_view.select_single(
+            ubuntuuitoolkit.Standard, objectName='listitem_standard')
+        self.assertTrue(self._item.exists())
+
+    def test_supported_class(self):
+        self.assertTrue(issubclass(
+            ubuntuuitoolkit.Base, ubuntuuitoolkit.Empty))
+        self.assertTrue(issubclass(
+            ubuntuuitoolkit.ItemSelector, ubuntuuitoolkit.Empty))
+        self.assertTrue(issubclass(
+            ubuntuuitoolkit.Standard, ubuntuuitoolkit.Empty))
+        self.assertTrue(issubclass(
+            ubuntuuitoolkit.SingleControl, ubuntuuitoolkit.Empty))
+        self.assertTrue(issubclass(
+            ubuntuuitoolkit.MultiValue, ubuntuuitoolkit.Base))
+        self.assertTrue(issubclass(
+            ubuntuuitoolkit.SingleValue, ubuntuuitoolkit.Base))
+        self.assertTrue(issubclass(
+            ubuntuuitoolkit.Subtitled, ubuntuuitoolkit.Base))
+
+    def test_standard_custom_proxy_object(self):
+        self.assertIsInstance(self._item, ubuntuuitoolkit.Standard)
+
+    def test_swipe_item(self):
+        self._item.swipe_to_delete()
+        self.assertTrue(self._item.waitingConfirmationForRemoval)
+
+    def test_swipe_item_to_right(self):
+        self._item.swipe_to_delete('right')
+        self.assertTrue(self._item.waitingConfirmationForRemoval)
+
+    def test_swipe_item_to_left(self):
+        self._item.swipe_to_delete('left')
+        self.assertTrue(self._item.waitingConfirmationForRemoval)
+
+    def test_swipe_item_to_wrong_direction(self):
+        self.assertRaises(
+            ubuntuuitoolkit.ToolkitException,
+            self._item.swipe_to_delete, 'up')
+
+    def test_delete_item_moving_right(self):
+        self._item.swipe_to_delete('right')
+        self._item.confirm_removal()
+        self.assertFalse(self._item.exists())
+
+    def test_delete_item_moving_left(self):
+        self._item.swipe_to_delete('left')
+        self._item.confirm_removal()
+        self.assertFalse(self._item.exists())
+
+    def test_delete_non_removable_item(self):
+        self._item = self.main_view.select_single(
+            ubuntuuitoolkit.Empty, objectName='listitem_empty')
+        self.assertRaises(
+            ubuntuuitoolkit.ToolkitException, self._item.swipe_to_delete)
+
+    def test_confirm_removal_when_item_was_not_swiped(self):
+        self.assertRaises(
+            ubuntuuitoolkit.ToolkitException, self._item.confirm_removal)
+
+    def test_delete_item_without_confirm(self):
+        item = self.main_view.select_single(
+            ubuntuuitoolkit.Standard, objectName='listitem_without_confirm')
+        item.swipe_to_delete()
+        self.assertFalse(item.exists())
+
+    def test_delete_item_with_confirmation_that_will_be_destroyed(self):
+        item = self.main_view.select_single(
+            ubuntuuitoolkit.Standard,
+            objectName='listitem_destroyed_on_remove_with_confirm')
+        item.swipe_to_delete()
+        item.confirm_removal()
+        self.assertFalse(item.exists())
+
+    def test_delete_item_without_confirmation_that_will_be_destroyed(self):
+        item = self.main_view.select_single(
+            ubuntuuitoolkit.Standard,
+            objectName='listitem_destroyed_on_remove_without_confirm')
+        item.swipe_to_delete()
+        self.assertFalse(item.exists())
---
+++ tests/autopilot/ubuntuuitoolkit/tests/custom_proxy_objects/test_main_view.py
@@ -0,0 +1,132 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+#
+# Copyright (C) 2013, 2014 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+import ubuntuuitoolkit
+from ubuntuuitoolkit import tests
+
+
+class MainViewTestCase(tests.QMLStringAppTestCase):
+
+    test_qml = ("""
+import QtQuick 2.0
+import Ubuntu.Components 0.1
+
+MainView {
+    width: units.gu(48)
+    height: units.gu(60)
+}
+""")
+
+    def test_main_view_custom_emulator(self):
+        self.assertIsInstance(self.main_view, ubuntuuitoolkit.MainView)
+
+    def test_get_header_without_header(self):
+        header = self.main_view.get_header()
+        self.assertFalse(header.visible)
+
+    def test_toolbar_custom_emulator(self):
+        toolbar = self.main_view.get_toolbar()
+        self.assertIsInstance(toolbar, ubuntuuitoolkit.Toolbar)
+
+    def test_open_toolbar(self):
+        with mock.patch.object(ubuntuuitoolkit.Toolbar, 'open') as mock_open:
+            self.main_view.open_toolbar()
+
+        mock_open.assert_called_once_with()
+
+    def test_close_toolbar(self):
+        with mock.patch.object(ubuntuuitoolkit.Toolbar, 'close') as mock_close:
+            self.main_view.close_toolbar()
+
+        mock_close.assert_called_once_with()
+
+    def test_open_toolbar_returns_the_toolbar(self):
+        toolbar = self.main_view.open_toolbar()
+        self.assertIsInstance(toolbar, ubuntuuitoolkit.Toolbar)
+
+    def test_get_tabs_without_tabs(self):
+        error = self.assertRaises(
+            ubuntuuitoolkit.ToolkitException, self.main_view.get_tabs)
+        self.assertEqual(
+            str(error), 'The MainView has no Tabs.')
+
+    def test_switch_to_next_tab_without_tabs(self):
+        header = self.main_view.get_header()
+        error = self.assertRaises(
+            ubuntuuitoolkit.ToolkitException,
+            header.switch_to_next_tab)
+        self.assertEqual(
+            str(error), 'The MainView has no Tabs.')
+
+
+class GoBackTestCase(tests.QMLStringAppTestCase):
+
+    test_qml = ("""
+import QtQuick 2.0
+import Ubuntu.Components 0.1
+
+MainView {
+    width: units.gu(48)
+    height: units.gu(60)
+
+    PageStack {
+        id: pageStack
+        Component.onCompleted: push(page0)
+
+        Page {
+            id: page0
+            title: "Page 0"
+            visible: false
+
+            Button {
+                objectName: "go_to_page1"
+                text: "Go to page 1"
+                onClicked: pageStack.push(page1)
+            }
+        }
+
+        Page {
+            id: page1
+            title: "Page 1"
+            visible: false
+        }
+    }
+}
+""")
+
+    def setUp(self):
+        super(GoBackTestCase, self).setUp()
+        self.header = self.main_view.get_header()
+        self.assertEqual(self.header.title, 'Page 0')
+
+    def test_open_page(self):
+        self._go_to_page1()
+        self.assertEqual(self.header.title, 'Page 1')
+
+    def _go_to_page1(self):
+        button = self.main_view.select_single(
+            'Button', objectName='go_to_page1')
+        self.pointing_device.click_object(button)
+
+    def test_go_back(self):
+        self._go_to_page1()
+        self.main_view.go_back()
+        self.assertEqual(self.header.title, 'Page 0')
