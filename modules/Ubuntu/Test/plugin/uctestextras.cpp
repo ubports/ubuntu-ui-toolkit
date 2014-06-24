e2d04ecb99d4ac6e3a6a5d4737ef0527b69dda67
Zsombor Egri <zsombor.egri@canonical.com>
2014-06-24 15:04:10 +0300

    unwanted changes reverted

--- modules/Ubuntu/Test/plugin/uctestextras.cpp
+++ modules/Ubuntu/Test/plugin/uctestextras.cpp
@@ -187,16 +187,17 @@ void UCTestExtras::touchDrag(int touchId, QQuickItem *item, const QPoint &from, 
     }
     touchPress(touchId, item, from);
     QTest::qWait(10);
+    touchMove(touchId, item, from);
     QPoint movePoint(from);
     qreal stepDx = delta.x() / steps;
     qreal stepDy = delta.y() / steps;
     if (!delta.isNull()) {
-        for (int i = 0; i < steps; i++) {
+        for (int i = 0; i < steps - 1; i++) {
             QTest::qWait(10);
             movePoint += QPoint(stepDx, stepDy);
             touchMove(touchId, item, movePoint);
         }
     }
     QTest::qWait(10);
-    touchRelease(touchId, item, movePoint);
+    touchRelease(touchId, item, from + delta);
 }
