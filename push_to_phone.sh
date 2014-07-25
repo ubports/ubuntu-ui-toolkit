3ad62757cd0af91f6d35f28abd0917db1db1c351
Christian Dywan <christian.dywan@canonical.com>
2014-07-25 12:42:34 +0200

    Also push the versioned 10 and 11 folders to the phone

--- push_to_phone.sh
+++ push_to_phone.sh
@@ -31,7 +31,7 @@ for i in $(ls Ubuntu/Components/*.js 2>/dev/null); do
     adb push $i /usr/lib/$ARCH/qt5/qml/Ubuntu/Components/
 done
 cd ..
-for i in ListItems Pickers Popups Colors Styles Themes artwork; do
+for i in 10 11 ListItems Pickers Popups Colors Styles Themes artwork; do
     adb push modules/Ubuntu/Components/$i/ /usr/lib/$ARCH/qt5/qml/Ubuntu/Components/$i || exit 1
 done
 # Autopilot tests should always match the Toolkit
