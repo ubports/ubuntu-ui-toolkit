d4f47163f799c6d83383c30d434abbdc4622a043
Omer Akram <om26er@ubuntu.com>
2014-07-28 21:23:01 +0500

    flake8

--- tests/autopilot/ubuntuuitoolkit/tests/custom_proxy_objects/test_textarea.py
+++ tests/autopilot/ubuntuuitoolkit/tests/custom_proxy_objects/test_textarea.py
@@ -14,13 +14,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-try:
-    from unittest import mock
-except ImportError:
-    import mock
-
-from autopilot import platform
-
 import ubuntuuitoolkit
 from ubuntuuitoolkit import tests
 
