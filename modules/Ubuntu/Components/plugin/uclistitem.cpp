bc3236570890c3b7cbb009facfafddb893a62424
Zsombor Egri <zsombor.egri@canonical.com>
2014-09-15 15:59:16 +0300

    documentation and versioning fix

--- modules/Ubuntu/Components/plugin/uclistitem.cpp
+++ modules/Ubuntu/Components/plugin/uclistitem.cpp
@@ -195,8 +195,9 @@ void UCListItemPrivate::listenToRebind(bool listen)
 /*!
  * \qmltype ListItem
  * \instantiates UCListItem
- * \inqmlmodule Ubuntu.Components 1.1
- * \ingroup ubuntu
+ * \inqmlmodule Ubuntu.Components 1.2
+ * \ingroup unstable-ubuntu-listitems
+ * \since Ubuntu.Components 1.2
  * \brief The ListItem element provides Ubuntu design standards for list or grid
  * views.
  *
