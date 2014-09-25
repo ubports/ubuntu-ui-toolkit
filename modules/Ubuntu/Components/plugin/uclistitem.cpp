c316d300c10c50a687cad3fd53adcb21a24a48e5
Zsombor Egri <zsombor.egri@canonical.com>
2014-09-25 16:52:59 +0300

    panelItem removed, added as resource data into ListItemActions

--- modules/Ubuntu/Components/plugin/uclistitem.cpp
+++ modules/Ubuntu/Components/plugin/uclistitem.cpp
@@ -700,6 +700,9 @@ void UCListItem::mouseReleaseEvent(QMouseEvent *event)
             } else {
                 d->reboundTo(snapPosition);
             }
+            // unset dragging in panel item
+            UCListItemActionsPrivate::setDragging(d->leadingActions, this, false);
+            UCListItemActionsPrivate::setDragging(d->trailingActions, this, false);
         }
     }
     d->setPressed(false);
@@ -740,6 +743,9 @@ void UCListItem::mouseMoveEvent(QMouseEvent *event)
             // block flickable
             d->setMoved(true);
             d->contentItem->setX(x);
+            // set dragging in panel item
+            UCListItemActionsPrivate::setDragging(d->leadingActions, this, true);
+            UCListItemActionsPrivate::setDragging(d->trailingActions, this, true);
         }
     }
 }
