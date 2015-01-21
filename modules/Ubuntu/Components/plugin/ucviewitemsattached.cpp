a611d5baff2084be3602957e09024c1d34b21929
Zsombor Egri <zsombor.egri@canonical.com>
2015-01-21 19:19:47 +0200

    documentation and API fixed

--- modules/Ubuntu/Components/plugin/ucviewitemsattached.cpp
+++ modules/Ubuntu/Components/plugin/ucviewitemsattached.cpp
@@ -76,11 +76,9 @@ void UCViewItemsAttachedPrivate::buildFlickablesList()
 void UCViewItemsAttachedPrivate::clearChangesList()
 {
     // clear property change objects
-    Q_FOREACH(PropertyChange *change, changes) {
-        // deleting PropertyChange will restore the saved property
-        // to its original binding/value
-        delete change;
-    }
+    // deleting PropertyChange will restore the saved property
+    // to its original binding/value
+    qDeleteAll(changes);
     changes.clear();
 }
 
