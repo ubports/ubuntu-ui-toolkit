ce7c7ddd3348248665bb9bd48fa2da6f775abbd9
Christian Dywan <christian.dywan@canonical.com>
2015-01-30 12:58:40 +0100

    Smarter arg handling and usage without args

--- run_tests.sh
+++ run_tests.sh
@@ -22,6 +22,13 @@ if [ ! -e $QML2_IMPORT_PATH/Ubuntu/Layouts/libUbuntuLayouts.so ]; then
     exit 1
 fi
 
+MODES='run|list|vis|launch'
+usage(){
+    echo Usage:
+    echo "  " $(basename $0) "[-h] [$MODES] SUITE"
+    exit 1
+}
+
 cd tests/autopilot
 
 export UBUNTU_UI_TOOLKIT_AUTOPILOT_FROM_SOURCE=1
@@ -29,25 +36,36 @@ export UITK_BUILD_ROOT="$BUILD_DIR"
 export UITK_SOURCE_ROOT="$SRC_DIR"
 MODE="run"
 SUITE=ubuntuuitoolkit
-if [ "$1" != "" -a "$2" != "" ]; then
-    MODE="$1"
-    SUITE="$2"
-    shift 2
-elif [ "$1" != "" ]; then
-    MODE="$1"
-    shift 1
+
+if [ "$1" == "" ]; then
+    usage
+elif [[ $MODES == *$1* ]]; then
+    MODE=$1
+    if [ "$2" == "" ]; then
+        set "$SUITE"
+    else
+        shift 1
+    fi
+elif [ "$2" = "" ]; then
+    true
+elif [[ $MODES == *$2* ]]; then
+    MODE=$2
+    if [ "$3" == "" ]; then
+        shift 1
+        set "$SUITE"
+    else
+        shift 2
+    fi
 fi
 
 if [ "$MODE" = "run" ]; then
-    autopilot3 $MODE -o ../../$SUITE -f xml -r -rd ../../ $SUITE $*
+    autopilot3 $MODE -o ../../$SUITE -f xml -r -rd ../../ $*
 else
-    autopilot3 $MODE $SUITE $*
+    autopilot3 $MODE $*
 fi
 
 RESULT=$?
 if [ "$RESULT" == "2" ]; then
-    echo Usage:
-    echo "  " $(basename $0) "run|list|vis|launch [suite] [-v]"
-    echo "See also 'autopilot -h'"
+    usage
 fi
 exit $RESULT
