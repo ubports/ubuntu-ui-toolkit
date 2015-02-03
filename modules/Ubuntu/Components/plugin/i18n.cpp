9fc57814222a390924d13adf74ea47289a937505
Christian Dywan <christian.dywan@canonical.com>
2015-02-03 11:00:42 +0100

    Add QML example of UserMetrics to i18n.tag documentation

--- modules/Ubuntu/Components/plugin/i18n.cpp
+++ modules/Ubuntu/Components/plugin/i18n.cpp
@@ -218,6 +218,23 @@ QString UbuntuI18n::dctr(const QString& domain, const QString& context, const QS
  * \qmlmethod string i18n::tag(string text)
  * Mark \a text for translation at a later point. Typically this allows an API
  * to take the original string and pass it to dtr (or dgettext).
+ *
+ * \qml
+ * import QtQuick 2.0
+ * import UserMetrics 0.1
+ *
+ * Metric {
+ *     name: "distance"
+ *     format: i18n.tag("Distance covered today: %1 km")
+ *     emptyFormat: i18n.tag("No running today")
+ *     domain: "runner.forest"
+ * }
+ * \endqml
+ *
+ * The strings tagged for localzation above are passed to the implementation
+ * of UserMetrics verbatim, as well as the domain of the app. Display and
+ * translation of the strings will happen in the lockscreen, where the same
+ * strings will be passed to i18n.tr.
  */
 QString UbuntuI18n::tag(const QString& text)
 {
