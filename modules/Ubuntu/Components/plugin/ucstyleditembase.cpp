ac8a846e96806181d873bb12b365db79959babd1
Loïc Molinari <loic.molinari@canonical.com>
2015-04-15 11:53:18 +0200

    Fixed build.

--- modules/Ubuntu/Components/plugin/ucstyleditembase.cpp
+++ modules/Ubuntu/Components/plugin/ucstyleditembase.cpp
@@ -58,7 +58,6 @@ void UCStyledItemBasePrivate::setFocusable(bool focus)
 
 bool UCStyledItemBasePrivate::isParentFocusable()
 {
-    Q_Q(UCStyledItemBase);
     if (!activeFocusOnPress) {
         return false;
     }
