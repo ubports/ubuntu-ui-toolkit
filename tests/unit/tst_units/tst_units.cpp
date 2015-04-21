6a79ea967e7dbc8d80f937ac1ba6ab3392605988
Gerry Boland <gerry.boland@canonical.com>
2015-04-21 12:23:44 +0100

    Add tests to ensure units.gu and units.dp are being calculated correctly

--- tests/unit/tst_units/tst_units.cpp
+++ tests/unit/tst_units/tst_units.cpp
@@ -25,6 +25,10 @@ class tst_UCUnits: public QObject
 
 private Q_SLOTS:
 
+    void cleanup() {
+        qunsetenv("QT_DEVICE_PIXEL_RATIO");
+    }
+
     void singletonInstance() {
         UCUnits& units1 = UCUnits::instance();
         UCUnits& units2 = UCUnits::instance();
@@ -151,6 +155,123 @@ private Q_SLOTS:
         QCOMPARE(units.dp(0.51), 1.0f);
         QCOMPARE(units.dp(0.9999), 2.0f);
         QCOMPARE(units.dp(1000.01), 2500.0f);
+    }
+
+    void devicePixelRatioReadFromEnvVar() {
+        qputenv("QT_DEVICE_PIXEL_RATIO", "2");
+        UCUnits units;
+
+        QCOMPARE(units.devicePixelRatio(), 2.0f);
+    }
+
+    void dpGridUnitDefaultWithDPR2() {
+        qputenv("QT_DEVICE_PIXEL_RATIO", "2");
+        UCUnits units;
+
+        QCOMPARE(units.dp(1.0), 0.5f);
+        QCOMPARE(units.dp(1.32), 0.5f);
+        QCOMPARE(units.dp(1.72), 1.0f);
+        QCOMPARE(units.dp(0.23), 0.0f);
+        QCOMPARE(units.dp(0.51), 0.5f);
+        QCOMPARE(units.dp(0.9999), 0.5f);
+        QCOMPARE(units.dp(1000.01), 500.0f);
+    }
+
+    void guGridUnitDefaultWithDPR2() {
+        qputenv("QT_DEVICE_PIXEL_RATIO", "2");
+        UCUnits units;
+
+        QCOMPARE(units.gu(1.0), 4.0f);
+        QCOMPARE(units.gu(1.32), 5.5f);
+        QCOMPARE(units.gu(1.72), 7.0f);
+        QCOMPARE(units.gu(0.23), 1.0f);
+        QCOMPARE(units.gu(0.51), 2.0f);
+        QCOMPARE(units.gu(0.9999), 4.0f);
+        QCOMPARE(units.gu(1000.01), 4000.0f);
+    }
+
+    void dpGridUnitSixteenWithDPR2() {
+        qputenv("QT_DEVICE_PIXEL_RATIO", "2");
+        UCUnits units;
+        units.setGridUnit(16);
+
+        QCOMPARE(units.dp(1.0), 1.0f);
+        QCOMPARE(units.dp(1.32), 1.5f);
+        QCOMPARE(units.dp(1.72), 1.5f);
+        QCOMPARE(units.dp(0.23), 0.0f);
+        QCOMPARE(units.dp(0.51), 0.5f);
+        QCOMPARE(units.dp(0.9999), 1.0f);
+        QCOMPARE(units.dp(1000.01), 1000.0f);
+    }
+
+    void guGridUnitSixteenWithDPR2() {
+        qputenv("QT_DEVICE_PIXEL_RATIO", "2");
+        UCUnits units;
+        units.setGridUnit(16);
+
+        QCOMPARE(units.gu(0.5), 4.0f);
+        QCOMPARE(units.gu(1), 8.0f);
+        QCOMPARE(units.gu(1.5), 12.0f);
+        QCOMPARE(units.gu(2), 16.0f);
+        QCOMPARE(units.gu(4), 32.0f);
+        QCOMPARE(units.gu(100000), 800000.0f);
+        QCOMPARE(units.gu(150.51983), 1204.0f);
+    }
+
+    void dpGridUnitTwentyWithDPR2() {
+        qputenv("QT_DEVICE_PIXEL_RATIO", "2");
+        UCUnits units;
+        units.setGridUnit(20);
+
+        QCOMPARE(units.dp(1.0), 1.0f);
+        QCOMPARE(units.dp(1.32), 1.0f);
+        QCOMPARE(units.dp(1.72), 2.0f);
+        QCOMPARE(units.dp(0.23), 0.0f);
+        QCOMPARE(units.dp(0.51), 1.0f);
+        QCOMPARE(units.dp(0.9999), 1.0f);
+        QCOMPARE(units.dp(1000.01), 1250.0f);
+    }
+
+    void guGridUnitTenWithDPR2() {
+        qputenv("QT_DEVICE_PIXEL_RATIO", "2");
+        UCUnits units;
+        units.setGridUnit(10);
+
+        QCOMPARE(units.gu(0.5), 2.5f);
+        QCOMPARE(units.gu(1), 5.0f);
+        QCOMPARE(units.gu(1.5), 7.5f);
+        QCOMPARE(units.gu(2), 10.0f);
+        QCOMPARE(units.gu(4), 20.0f);
+        QCOMPARE(units.gu(100000), 500000.0f);
+        QCOMPARE(units.gu(150.51983), 752.5f);
+    }
+
+    void dpGridUnitSixteenWithDPR3() {
+        qputenv("QT_DEVICE_PIXEL_RATIO", "3");
+        UCUnits units;
+        units.setGridUnit(16);
+
+        QCOMPARE(units.dp(1.0), 0.666666f);
+        QCOMPARE(units.dp(1.32), 1.0f);
+        QCOMPARE(units.dp(1.72), 1.0f);
+        QCOMPARE(units.dp(0.23), 0.0f);
+        QCOMPARE(units.dp(0.51), 0.333333f);
+        QCOMPARE(units.dp(0.9999), 0.666666f);
+        QCOMPARE(units.dp(1000.01), 666.666666f);
+    }
+
+    void guGridUnitSixteenWithDPR3() {
+        qputenv("QT_DEVICE_PIXEL_RATIO", "3");
+        UCUnits units;
+        units.setGridUnit(16);
+
+        QCOMPARE(units.gu(0.5), 2.666666f);
+        QCOMPARE(units.gu(1), 5.333333f);
+        QCOMPARE(units.gu(1.5), 8.0f);
+        QCOMPARE(units.gu(2), 10.666666f);
+        QCOMPARE(units.gu(4), 21.333333f);
+        QCOMPARE(units.gu(100000), 533333.333333f);
+        QCOMPARE(units.gu(150.51983), 802.666666f);
     }
 
     void resolveEmpty() {
