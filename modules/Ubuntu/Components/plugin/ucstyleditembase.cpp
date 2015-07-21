acbc3fe74c4ffb6f26377ca11219eb4122d16834
Christian Dywan <christian.dywan@canonical.com>
2015-07-21 11:43:20 -0400

    Enable (Shift)Tab via activeFocusOnTab

--- modules/Ubuntu/Components/plugin/ucstyleditembase.cpp
+++ modules/Ubuntu/Components/plugin/ucstyleditembase.cpp
@@ -203,6 +203,8 @@ void UCStyledItemBase::setActiveFocusOnPress(bool value)
         return;
     d->activeFocusOnPress = value;
     d->setFocusable(d->activeFocusOnPress);
+    // https://bugreports.qt.io/browse/QTBUG-40043
+    setProperty("activeFocusOnTab", value);
     Q_EMIT activeFocusOnPressChanged();
 }
 
