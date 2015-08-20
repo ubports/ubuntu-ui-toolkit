039930c5e0ed1258ca9beea70d0a961da21ccb6c
Zsombor Egri <zsombor.egri@canonical.com>
2015-08-20 09:57:24 +0300

    roll back commented performance test

--- tests/unit/tst_performance/tst_performance.cpp
+++ tests/unit/tst_performance/tst_performance.cpp
@@ -116,7 +116,7 @@ private Q_SLOTS:
         QTest::newRow("list with new ListItem with actions") << "ListItemWithActionsList.qml" << QUrl();
         QTest::newRow("list with new ListItem with inline actions") << "ListItemWithInlineActionsList.qml" << QUrl();
         QTest::newRow("list with Captions, preset: caption") << "ListOfCaptions.qml" << QUrl();
-//        QTest::newRow("list with ListItems.Empty (equivalent to the new ListItem") << "ListItemsEmptyList.qml" << QUrl();
+        QTest::newRow("list with ListItems.Empty (equivalent to the new ListItem") << "ListItemsEmptyList.qml" << QUrl();
         // disable this test as it takes >20 seconds. Kept still for measurements to be done during development
 //        QTest::newRow("list with ListItems.Base (one icon, one label and one chevron)") << "ListItemsBaseList.qml" << QUrl();
         QTest::newRow("single MainView") << "MainView.qml" << QUrl();
