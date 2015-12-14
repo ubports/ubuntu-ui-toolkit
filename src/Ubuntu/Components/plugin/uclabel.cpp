9560cc229253d8e4fe35220279dec78fc86686e1
Zsombor Egri <zsombor.egri@canonical.com>
2015-12-14 11:29:22 +0200

    fix Label's default color

--- src/Ubuntu/Components/plugin/uclabel.cpp
+++ src/Ubuntu/Components/plugin/uclabel.cpp
@@ -108,7 +108,7 @@ void UCLabel::postThemeChanged()
     if (m_flags & ColorSet) {
         return;
     }
-    setColor(theme->getPaletteColor("selected", "backgroundText"));
+    setColor(theme->getPaletteColor("normal", "baseText"));
     m_flags &= ~ColorSet;
 }
 
