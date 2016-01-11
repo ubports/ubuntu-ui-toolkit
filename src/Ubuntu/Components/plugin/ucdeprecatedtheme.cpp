5f8f6c90852ddf16680d87c17a932291b645fb5a
Zsombor Egri <zsombor.egri@canonical.com>
2016-01-11 19:18:51 +0200

    rolling back unwanted changes

--- src/Ubuntu/Components/plugin/ucdeprecatedtheme.cpp
+++ src/Ubuntu/Components/plugin/ucdeprecatedtheme.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Canonical Ltd.
+ * Copyright 2016 Canonical Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -163,4 +163,3 @@ void UCDeprecatedTheme::registerToContext(QQmlContext* context)
     QObject::connect(oldTheme, SIGNAL(nameChanged()),
                      themeChangeListener, SLOT(updateContextProperty()));
 }
-
--- src/Ubuntu/Components/plugin/ucstyleditembase.cpp
+++ src/Ubuntu/Components/plugin/ucstyleditembase.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Canonical Ltd.
+ * Copyright 2015 Canonical Ltd.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
@@ -139,7 +139,6 @@ UCStyledItemBase::UCStyledItemBase(UCStyledItemBasePrivate &dd, QQuickItem *pare
     Q_D(UCStyledItemBase);
     d->init();
 }
-
 
 /*!
  * \qmlmethod void StyledItemBase::requestFocus(Qt::FocusReason reason)
