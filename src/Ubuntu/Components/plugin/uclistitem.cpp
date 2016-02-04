1ee9c7116e77ee920eb529718ed3d8be9a33fe60
Christian Dywan <christian.dywan@canonical.com>
2016-02-04 18:17:30 +0100

    Suppress click signal if releasing outside of ListItem

--- src/Ubuntu/Components/plugin/uclistitem.cpp
+++ src/Ubuntu/Components/plugin/uclistitem.cpp
@@ -1250,6 +1250,8 @@ void UCListItem::mouseReleaseEvent(QMouseEvent *event)
     }
 
     UCStyledItemBase::mouseReleaseEvent(event);
+    if (!d->contentItem->contains(d->contentItem->mapFromScene(event->windowPos())))
+        d->suppressClick = true;
     d->handleLeftButtonRelease(event);
 }
 
