5858efc7d0ac675a5a78de21b4816f24ed0a0e86
Christian Dywan <christian.dywan@canonical.com>
2016-02-09 14:15:19 +0100

    Skip flaky tst_mousefilterTest::doubleClicked

--- tests/unit_x11/tst_mousefilters/tst_mousefilterstest.cpp
+++ tests/unit_x11/tst_mousefilters/tst_mousefilterstest.cpp
@@ -544,6 +544,7 @@ private Q_SLOTS:
 
     void testCase_doubleClicked()
     {
+        QSKIP("FIXME: lp#1542215 Flaky test result");
         QScopedPointer<QQuickView> view(loadTest("DoubleClicked.qml"));
         QVERIFY(view);
         UCMouse *filter = attachedFilter<UCMouse>(view->rootObject(), "FilterOwner");
