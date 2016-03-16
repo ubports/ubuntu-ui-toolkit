4a69cb3e79bb952bf6f5fe8dc9f554a3cf939b4d
Albert Astals Cid <albert.astals@canonical.com>
2016-03-16 15:24:15 +0100

    Some string optimizations

--- src/Ubuntu/Components/plugin/unitythemeiconprovider.cpp
+++ src/Ubuntu/Components/plugin/unitythemeiconprovider.cpp
@@ -107,7 +107,7 @@ private:
                 }
 
                 Q_FOREACH(const QString &name, settings.value("Icon Theme/Inherits").toStringList()) {
-                    if (name != "hicolor") {
+                    if (name != QLatin1String("hicolor")) {
                         parents.append(IconTheme::get(name));
                     }
                 }
@@ -120,11 +120,11 @@ private:
 
     SizeType sizeTypeFromString(const QString &string)
     {
-        if (string == "Fixed")
+        if (string == QLatin1String("Fixed"))
             return Fixed;
-        if (string == "Scalable")
+        if (string == QLatin1String("Scalable"))
             return Scalable;
-        if (string == "Threshold")
+        if (string == QLatin1String("Threshold"))
             return Threshold;
         qWarning() << "IconTheme: unknown size type '" << string << "'. Assuming 'Fixed'.";
         return Fixed;
@@ -137,14 +137,14 @@ private:
         const bool anyZero = size.width() <= 0 || size.height() <= 0;
         const Qt::AspectRatioMode scaleMode = anyZero ? Qt::KeepAspectRatioByExpanding : Qt::KeepAspectRatio;
 
-        if (filename.endsWith(".png")) {
+        if (filename.endsWith(QLatin1String(".png"))) {
             pixmap = QPixmap(filename);
             if (!pixmap.isNull() && !size.isNull() && (pixmap.width() != size.width() || pixmap.height() != size.height())) {
                 const QSize newSize = pixmap.size().scaled(size.width(), size.height(), scaleMode);
                 pixmap = pixmap.scaled(newSize);
             }
         }
-        else if (filename.endsWith(".svg")) {
+        else if (filename.endsWith(QLatin1String(".svg"))) {
             QSvgRenderer renderer(filename);
             pixmap = QPixmap(renderer.defaultSize().scaled(size.width(), size.height(), scaleMode));
             pixmap.fill(Qt::transparent);
@@ -158,8 +158,8 @@ private:
 
     QString lookupIconFile(const QString &dir, const QString &name)
     {
-        QString png = QString("%1/%2.png").arg(dir).arg(name);
-        QString svg = QString("%1/%2.svg").arg(dir).arg(name);
+        QString png = QStringLiteral("%1/%2.png").arg(dir, name);
+        QString svg = QStringLiteral("%1/%2.svg").arg(dir, name);
 
         Q_FOREACH(const QString &baseDir, baseDirs) {
             QString filename = baseDir + "/" + png;
@@ -268,11 +268,11 @@ QPixmap UnityThemeIconProvider::requestPixmap(const QString &id, QSize *size, co
     // The hicolor theme will be searched last as per
     // https://specifications.freedesktop.org/icon-theme-spec/icon-theme-spec-latest.html
     QSet<QString> alreadySearchedThemes;
-    const QStringList names = id.split(",", QString::SkipEmptyParts);
+    const QStringList names = id.split(QLatin1Char(','), QString::SkipEmptyParts);
     QPixmap pixmap = theme->findBestIcon(names, requestedSize, &alreadySearchedThemes);
 
     if (pixmap.isNull()) {
-        IconTheme::IconThemePointer theme = IconTheme::get("hicolor");
+        IconTheme::IconThemePointer theme = IconTheme::get(QStringLiteral("hicolor"));
         return theme->findBestIcon(names, requestedSize, nullptr);
     }
 
