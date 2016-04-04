b22fd45b96b9bbf148ba2b12c700cf62eb222b12
Zsombor Egri <zsombor.egri@canonical.com>
2016-04-04 13:16:08 +0300

    missing license header added

--- src/Ubuntu/UbuntuToolkit/mousetouchadaptor.cpp
+++ src/Ubuntu/UbuntuToolkit/mousetouchadaptor.cpp
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Authored by: Daniel d'Andrada <daniel.dandrada@canonical.com>
+ *              Zsombor Egri <zsomboir.egri@canonical.com>
  */
 
 #include "mousetouchadaptor_p.h"
