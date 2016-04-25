ce1511f0573f74f192a07e66e8e056d3182d2072
Gerry Boland <gerry.boland@canonical.com>
2016-04-25 17:15:52 +0200

    UCLabel: should compare pixel size changes with previous font pixel size, not the default

--- src/Ubuntu/Components/plugin/uclabel.cpp
+++ src/Ubuntu/Components/plugin/uclabel.cpp
@@ -130,7 +130,7 @@ void UCLabelPrivate::init()
     q->postThemeChanged();
 
     updatePixelSize();
-    defaultFont = q->font();
+    QFont defaultFont = q->font();
     defaultFont.setFamily("Ubuntu");
     defaultFont.setWeight(QFont::Light);
     q->setFont(defaultFont);
@@ -202,7 +202,7 @@ void UCLabel::setFont2(const QFont &font)
     Q_D(UCLabel);
     // we must restrict ourself to the pixelSize change as any font property change will
     // lead to the setter call.
-    if (d->defaultFont.pixelSize() != font.pixelSize()) {
+    if (this->font().pixelSize() != font.pixelSize()) {
         d->flags |= UCLabelPrivate::PixelSizeSet;
     }
     QQuickText::setFont(font);
