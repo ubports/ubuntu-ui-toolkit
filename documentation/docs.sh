8662a238f4fe48f513f4873a235bd7f2c0d35c61
Christian Dywan <christian.dywan@canonical.com>
2016-07-18 13:08:32 +0200

    Include indexes in "offline" docs and filter out link errors

--- documentation/docs.sh
+++ documentation/docs.sh
@@ -28,7 +28,7 @@ mkdir -p $DOC_PATH
 # Offline docs for QtCreator
 eval "$QDOC $SRC/ubuntu-ui-toolkit-qtcreator.qdocconf 2> $DOC_PATH/qdoc.log"
 # FIXME: With Qt 5.2 this warning shows up, forcibly omit it from errors
-grep -v "error: HTML file already exists; overwriting" $DOC_PATH/qdoc.log | grep -v "qdoc: warning: No documentation for 'global'" > $DOC_PATH/qdoc.err
+grep -v "error: HTML file already exists; overwriting" $DOC_PATH/qdoc.log | grep -v "qdoc: warning: No documentation for 'global'" | grep -v "warning: Can't link to" > $DOC_PATH/qdoc.err
 cat $DOC_PATH/qdoc.err
 test ! -s $DOC_PATH/qdoc.err || exit 1
 echo docs: Offline done.
@@ -39,12 +39,19 @@ echo docs: qch done: $DOC_PATH
 # Online docs. Run qdoc twice: the second run with indexes for cross-referencing
 # other APIs but discard errors because qdoc inherits all doc bugs otherwise
 eval "$QDOC $SRC/ubuntu-ui-toolkit-online.qdocconf 2> $DOC_PATH/qdoc.log"
-grep -v "error: HTML file already exists; overwriting" $DOC_PATH/qdoc.log | grep -v "qdoc: warning: No documentation for 'global'" > $DOC_PATH/qdoc.err
+grep -v "error: HTML file already exists; overwriting" $DOC_PATH/qdoc.log | grep -v "qdoc: warning: No documentation for 'global'" | grep -v "warning: Can't link to" > $DOC_PATH/qdoc.err
 cat $DOC_PATH/qdoc.err
 test ! -s $DOC_PATH/qdoc.err || exit 1
 echo docs: Online done.
 
-eval "$QDOC $SRC/ubuntu-ui-toolkit-online-indexes.qdocconf 2> /dev/null"
+eval "$QDOC $SRC/ubuntu-ui-toolkit-offline-indexes.qdocconf 2> $DOC_PATH/qdoc.log"
+grep "warning: Can't link to" $DOC_PATH/qdoc.log > $DOC_PATH/qdoc.err
+cat $DOC_PATH/qdoc.err
+test ! -s $DOC_PATH/qdoc.err || exit 1
+eval "$QDOC $SRC/ubuntu-ui-toolkit-online-indexes.qdocconf 2> $DOC_PATH/qdoc.log"
+grep "warning: Can't link to" $DOC_PATH/qdoc.log > $DOC_PATH/qdoc.err
+cat $DOC_PATH/qdoc.err
+test ! -s $DOC_PATH/qdoc.err || exit 1
 echo docs: Cross-referenced done.
 
 sed -r -i 's@(</p>)<p/>@\1@g' $DOC_PATH/html/*.html
