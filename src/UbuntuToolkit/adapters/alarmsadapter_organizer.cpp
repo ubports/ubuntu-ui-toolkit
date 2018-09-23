6a5f9a0be39a3c44478f00a72f09175bf82418ce
Alberto Mardegan <mardy@users.sourceforge.net>
2018-09-23 15:52:47 +0300

    AlarmAdapter: use C++11 signal connection style

--- src/UbuntuToolkit/adapters/alarmsadapter_organizer.cpp
+++ src/UbuntuToolkit/adapters/alarmsadapter_organizer.cpp
@@ -454,9 +454,10 @@ void AlarmsAdapter::init()
     loadAlarms();
 
     // connect to manager to receive changes
-    QObject::connect(manager, SIGNAL(itemsModified(QList<QPair<QOrganizerItemId,QOrganizerManager::Operation> >)),
-                     this, SLOT(alarmOperation(QList<QPair<QOrganizerItemId,QOrganizerManager::Operation> >)));
-    QObject::connect(manager, SIGNAL(dataChanged()), this, SLOT(fetchAlarms()));
+    QObject::connect(manager, &QOrganizerManager::itemsModified,
+                     this, &AlarmsAdapter::alarmOperation);
+    QObject::connect(manager, &QOrganizerManager::dataChanged,
+                     this, &AlarmsAdapter::fetchAlarms);
 }
 
 AlarmsAdapter::~AlarmsAdapter()
@@ -638,7 +639,8 @@ bool AlarmsAdapter::fetchAlarms()
         fetchRequest->setFilter(filter);
 
         // start request
-        QObject::connect(fetchRequest, SIGNAL(stateChanged(QOrganizerAbstractRequest::State)), this, SLOT(completeFetchAlarms()));
+        QObject::connect(fetchRequest, &QOrganizerAbstractRequest::stateChanged,
+                         this, &AlarmsAdapter::completeFetchAlarms);
     }
 
     Q_EMIT q_ptr->alarmsRefreshStarted();
