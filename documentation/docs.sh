4276432f2c3b81606305db1920aae80092da34a1
Rodney Dawes <dobey@ubports.com>
2020-07-23 10:56:01 -0400

    Make docs build more lenient

--- documentation/docs.sh
+++ documentation/docs.sh
@@ -22,7 +22,6 @@
 # imports ASCII-wise). The result is that qdoc is unable to link to properties
 # inherited from UCMainViewBase like applicationName, so constructs like that
 # don't work: \l MainView::applicationName...
-set -x
 
 QDOC=$1
 QHELPGENERATOR=$2
@@ -37,7 +36,7 @@ mkdir -p $DOC_PATH
 # Offline docs for QtCreator
 eval "$QDOC $SRC/ubuntu-ui-toolkit-qtcreator.qdocconf 2> $DOC_PATH/qdoc.log"
 # FIXME: With Qt 5.2 this warning shows up, forcibly omit it from errors
-grep -v "error: Output file already exists; overwriting" $DOC_PATH/qdoc.log | grep -v "qdoc: warning: No documentation for 'global'" | grep -v "Maybe you meant" | grep -v "but there was no topic command" | grep -v " warning: " | grep -v "warning: Can't link to" > $DOC_PATH/qdoc.err
+grep -v "error: Output file already exists; overwriting" $DOC_PATH/qdoc.log | grep "error: " > $DOC_PATH/qdoc.err
 cat $DOC_PATH/qdoc.err
 test ! -s $DOC_PATH/qdoc.err || exit 1
 echo docs: Offline done.
@@ -47,7 +46,7 @@ echo docs: qch done: $DOC_PATH
 
 # Online docs
 eval "$QDOC $SRC/ubuntu-ui-toolkit-online.qdocconf 2> $DOC_PATH/qdoc.log"
-grep -v "error: HTML file already exists; overwriting" $DOC_PATH/qdoc.log | grep -v "qdoc: warning: No documentation for 'global'" | grep -v "warning: Can't link to" > $DOC_PATH/qdoc.err
+grep -v "error: Output file already exists; overwriting" $DOC_PATH/qdoc.log | grep "error: " > $DOC_PATH/qdoc.err
 cat $DOC_PATH/qdoc.err
 test ! -s $DOC_PATH/qdoc.err || exit 1
 echo docs: Online done.
